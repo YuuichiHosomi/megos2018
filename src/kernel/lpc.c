ac042d32553988cc3303d57fec58ed88fcd430e4
Nerry <108566+neri@users.noreply.github.com>
2019-10-26 20:41:48 +0900

    Cosmetic changes

--- src/kernel/lpc.c
+++ src/kernel/lpc.c
@@ -263,9 +263,6 @@ static int ps2_init() {
     uintptr_t size_of_buffer = 128;
     ps2_event_queue = moe_queue_create(size_of_buffer);
 
-    moe_install_irq(1, &ps2k_irq_handler);
-    moe_install_irq(12, &ps2m_irq_handler);
-
     if (ps2_wait_for_write(1) != 0) goto ps2_timeout_error;
     ps2_write_command(0x60);
     if (ps2_wait_for_write(1) != 0) goto ps2_timeout_error;
@@ -282,6 +279,8 @@ static int ps2_init() {
     if (ps2_wait_for_write(1) != 0) goto ps2_timeout_error;
     ps2_write_data(0xF4);
 
+    moe_install_irq(1, &ps2k_irq_handler);
+    moe_install_irq(12, &ps2m_irq_handler);
     moe_create_thread(&ps2_hid_thread, priority_realtime, 0, "ps2.hid");
 
     return 1;
